cf97a9080872f911d6cb3bc004a3473f2aee4e01
lgm243 <lgm243@gmail.com>
2016-10-30 16:56:06 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,6 +8,9 @@
 
 /*define('DISALLOW_FILE_EDIT',true);*/
 
+/*Masque les erreurs de connexion*/
+add_filter('login_errors',create_function('$a', "return 'Erreur';"));
+
 // 3) Masque les erreurs de connexion
 add_filter('login_errors',create_function('$a', "return 'Erreur';"));
 remove_action('wp_head', 'wp_generator');
@@ -17,10 +20,10 @@ remove_action('wp_head', 'index_rel_link');
 remove_action('wp_head', 'parent_post_rel_link');
 remove_action('wp_head', 'start_post_rel_link');
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
-remove_action('wp_head', 'wp_shortlink_wp_head');        // #4
-remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);    // #5
-add_filter('the_generator', '__return_false');            // #6
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );  // #8
+remove_action('wp_head', 'wp_shortlink_wp_head');        
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);   
+add_filter('the_generator', '__return_false');         
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); 
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
 /* JQUERY CSS*/
@@ -35,7 +38,7 @@ function wpEnqueueScripts(){
 add_action('wp_enqueue_scripts', 'wpEnqueueScripts');
 
 
-// Retire version ? script
+/*Retire version script*/
 function vc_remove_wp_ver_css_js( $src ) {
     if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) )
         $src = remove_query_arg( 'ver', $src );
@@ -50,8 +53,8 @@ register_nav_menus(array(
 'main-menu' => 'Menu Principal',
 ));
 
-
-
+/*editeur par default*/
+add_filter( 'wp_default_editor', create_function('', 'return "tinymce";') );
 
 /*lire la suite*/
 function wpdocs_excerpt_more( $more ) {
@@ -65,15 +68,58 @@ add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
 /* AJOUT TAILLE IMAGE*/
 add_theme_support( 'post-thumbnails' );
 if (function_exists('add_image_size')){
+    /*add_image_size( 'bandeau', 9999,  600, true );*/
 }
 
 /*REMOVE STYLE GALLERY WORDPRESS*/
 add_filter( 'use_default_gallery_style', '__return_false' );
 
-/*SUPPRIME LE SCOMMENTAIRES**/
+/*SUPPRIME LES COMMENTAIRES**/
 add_filter('comments_open', 'wpc_comments_closed', 10, 2);
 function wpc_comments_closed( $open, $post_id ) {
     $post = get_post( $post_id );
     $open = false;
     return $open;
+}
+
+/*PAGINATION*/
+function custom_pagination($numpages = '', $pagerange = '', $paged='') {
+  if (empty($pagerange)) {
+    $pagerange = 2;
+  }
+  global $paged;
+  if (empty($paged)) {
+    $paged = 1;
+  }
+  if ($numpages == '') {
+    global $wp_query;
+    $numpages = $wp_query->max_num_pages;
+    if(!$numpages) {
+        $numpages = 1;
+    }
+  }
+  $pagination_args = array(
+    'base'            => get_pagenum_link(1) . '%_%',
+    'format'          => 'page/%#%',
+    'total'           => $numpages,
+    'current'         => $paged,
+    'show_all'        => False,
+    'end_size'        => 1,
+    'mid_size'        => $pagerange,
+    'prev_next'       => True,
+    'prev_text'       => __('Previous'),
+    'next_text'       => __('Next'),
+    'type'            => 'plain',
+    'add_args'        => false,
+    'add_fragment'    => ''
+  );
+
+  $paginate_links = paginate_links($pagination_args);
+
+  if ($paginate_links) {
+      echo "<div class='pagination'>";
+      echo "<span class='pages'>Page " . $paged . " sur " . $numpages . "</span> ";
+      echo $paginate_links;
+    echo "</div>";
+  }
 }
