6b7a0922e8aef2b1d8361db882b28dccbef0b417
lgm243 <lgm243@gmail.com>
2017-01-06 16:56:10 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
 <!--[if IE]>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <![endif]-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 <meta name="format-detection" content="telephone=no" />
 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_directory'); ?>/style.min.css" />
 <!--[if lt IE 9]>
 <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
@@ -23,17 +20,13 @@
     <div class="grid">
             <a href="#" class="toggle" id="toggle"></a>
             <a href="<?= home_url(); ?>" class="logo">
-            <img src="<?php bloginfo("template_directory"); ?>/images/image.png" alt="<?php bloginfo("name"); ?>">
+            <img src="<?php bloginfo("template_directory"); ?>/images/les-fees-de-lassiette.png" alt="<?php bloginfo("name"); ?>">
             </a>
-            <nav role="navigation">
+            <nav>
             <?php wp_nav_menu( array(
-                                'theme_location' => 'nav',
+                                'theme_location' => 'main-menu',
                                 'container'      => false,
             )); ?>
             </nav>
     </div>
-</header>
-
-
-
-
+    </header>
